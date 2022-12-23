72d3fee24541a76331b43ec5cd306ef0fc2ce0f4
Lizvan27 <lizvanmandy@gmail.com>
2022-12-23 12:11:45 -0500

    S agrega actividad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { VistaComponent } from './body/vista/vista.component';
+import { TitleComponent } from './header/title/title.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleComponent,
+    VistaComponent
+  
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, TitleComponent, VistaComponent]
 })
 export class AppModule { }
